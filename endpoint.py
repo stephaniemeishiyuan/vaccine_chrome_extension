07d1fc0b32bbf6cb1ac57cd51aedf259520f54c9
Felicia Chen <Felicia@Felicias-MacBook-Pro.local>
2019-03-18 14:22:39 -0400

    read article

--- endpoint.py
+++ endpoint.py
@@ -4,6 +4,10 @@ from termcolor import colored
 from flask_cors import CORS
 import os
 
+# for getting article text
+from nltk import tokenize
+from goose3 import Goose
+
 prefix_q = '##### **Q:** '
 topk = 5
 import sys
@@ -17,7 +21,7 @@ CORS(app)
 @app.route("/receive", methods = ['POST'])
 def worker():
     data = request.get_json()
-    filepath = os.path.abspath('README2.md')
+    filepath = os.path.abspath('vaccine-myths.md')
     print(filepath)
     with open(filepath) as fp:
         questions = [v.replace(prefix_q, '').strip() for v in fp if v.strip() and v.startswith(prefix_q)]
@@ -27,6 +31,36 @@ def worker():
     topk_idx = np.argsort(score)[::-1][:topk]
     result = questions[topk_idx[0]]
     return jsonify({'message': result})
+
+# get entire article text
+@app.route("/article", methods = ['POST'])
+def article():
+    url = request.get_json()
+    filepath = os.path.abspath('vaccine-myths.md')
+    print(filepath)
+    with open(filepath) as fp:
+        myths = [v.replace(prefix_q, '').strip() for v in fp if v.strip() and v.startswith(prefix_q)]
+    with BertClient(port=5555, port_out=5556) as bc:
+        doc_vecs = bc.encode(myths)
+
+    # extract article text
+    g = Goose()
+    article = g.extract(url=url)
+    text = article.cleaned_text
+    sentences = tokenize.sent_tokenize(text)
+
+    # encode sentences
+    with BertClient(port=5555, port_out=5556) as bc:
+        article_encode = bc.encode(sentences)
+
+    # testing with 3rd sentence in article
+    score = np.sum(article_encode[3] * doc_vecs, axis=1)
+    topk_idx = np.argsort(score)[::-1][:topk]
+    result = questions[topk_idx[0]]
+    return jsonify({'message': result})
+
+
+
 
 if __name__ == "__main__":
 	app.run()
