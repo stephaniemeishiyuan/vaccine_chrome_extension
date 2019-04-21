f5e07ae622adde902dcfe10e947b364e5bd340fe
Stephanie Mei <sm546@duke.edu>
2019-04-21 15:27:06 -0400

    update classfication

--- js/background.js
+++ js/background.js
@@ -19,13 +19,17 @@ var test = {
     "id": 123,
     "is_tokenized": false
 };
+console.log(window.seltext+"aaa");
 
+var twosentence = {
+}
 function savetext(info,tab)
 {
     var jax = new XMLHttpRequest();
     jax.open("POST","http://0.0.0.0:8001/encode");
     jax.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     test["texts"] = [window.seltext];
+    twosentence["sentence1"] = window.seltext;
     console.log(test);
     jax.send(JSON.stringify(test));
     jax.onreadystatechange = function() { if(jax.readyState==4) {
@@ -33,14 +37,29 @@ function savetext(info,tab)
 }
 }
 }
-
 function display_result(json_result){
   var jax_to_endpoint = new XMLHttpRequest();
   jax_to_endpoint.open("POST","http://127.0.0.1:5000/receive");
   jax_to_endpoint.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   jax_to_endpoint.send(json_result);
-  jax_to_endpoint.onreadystatechange = function() { if(jax_to_endpoint.readyState==4) { alert(JSON.parse(jax_to_endpoint.response).message); }}
+  jax_to_endpoint.onreadystatechange = function() {
+    if(jax_to_endpoint.readyState==4)
+    { message = JSON.parse(jax_to_endpoint.response).message;
+      console.log(message);
+      send_check(message);
+    }}
 
+}
+
+function send_check(message){
+  twosentence["sentence2"] = message;
+  console.log(twosentence);
+  var jax_to_check = new XMLHttpRequest();
+  jax_to_check.open("POST","http://127.0.0.1:5000/check");
+  jax_to_check.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  jax_to_check.send(JSON.stringify(twosentence));
+  jax_to_check.onreadystatechange = function() { if(jax_to_check.readyState==4) {
+    alert(JSON.parse(jax_to_check.response).message);}}
 }
 
 var contexts = ["selection"];
